5bbb9ce2a282fd529dea93f9a2d66d58100b4fe4
Jualn <3430402714@qq.com>
2025-05-11 12:14:05 +0800

    fix: remove ignored files

--- smartCampus/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class SmartCampusConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'smartCampus'
-    verbose_name = 'Smart Campus' #改中文用的
